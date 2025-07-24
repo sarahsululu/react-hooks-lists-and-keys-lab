2e6b1880d99b1eb6c871fa8ffce94f4e516c3004
sarahsululu <sarahsululu47@gmail.com>
2025-07-24 10:41:38 +0300

    Complete NavBar, ProjectItem, and ProjectList components

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,16 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return (
+    <nav>
+      {links.map((link) => (
+        <a key={link} href={`#${link}`}>
+          {link.charAt(0).toUpperCase() + link.slice(1)}
+        </a>
+      ))}
+    </nav>
+  );
 }
 
 export default NavBar;
+
--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -6,10 +6,13 @@ function ProjectItem({ name, about, technologies }) {
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+        {technologies.map((tech) => (
+          <span key={tech}>{tech}</span>
+        ))}
       </div>
     </div>
   );
 }
 
 export default ProjectItem;
+
